9ec70e9cd33786befd37850db91b3f351e7b8091
Thomas Fritz <fritztho@gmail.com>
2012-09-21 23:29:13 +0200

    Modified Tom Wus lib to be working in node

--- tests/tomwu.js
+++ tests/tomwu.js
@@ -1,6 +1,5 @@
-var jsbn = require('../lib/jsbn');
-var jsbn2 = require('../lib/jsbn2');
-var prng4 = require('../lib/prng4');
-var rng = require('../lib/rng');
-var rsa = require('../lib/rsa');
-var rsa2 = require('../lib/rsa2');
+var RSAKey = require('../lib/rsa').RSAKey;
+
+var rsa = new RSAKey();
+console.log(rsa.generate(parseInt(2048),"10001"));
+console.log(rsa.encrypt("Test Message"));
