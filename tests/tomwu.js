2272078782dcf5e9518fcd4f03ee3fe271497c72
Thomas Fritz <fritztho@gmail.com>
2012-09-21 22:29:19 +0200

    Added Tom Wus http://www-cs-students.stanford.edu/~tjw/jsbn/ jsbn

---
+++ tests/tomwu.js
@@ -0,0 +1,6 @@
+var jsbn = require('../lib/jsbn');
+var jsbn2 = require('../lib/jsbn2');
+var prng4 = require('../lib/prng4');
+var rng = require('../lib/rng');
+var rsa = require('../lib/rsa');
+var rsa2 = require('../lib/rsa2');
