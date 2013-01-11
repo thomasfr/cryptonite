84c87fc50ae9edfe348b2df37427110e69901383
Thomas Fritz <fritztho@gmail.com>
2013-01-11 18:36:10 +0100

    Refactored and now using jade. Added dummy login and logout.

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var app = require('./lib/express');
 var server = require('./lib/httpServer')(app);
 var ws = require('./lib/websocketServer')(app, server);
-var routes = require('./lib/routes');
+var routes = require('./lib/routes')(app, server);
+var middleware = require('./lib/middleware')(app, server);
 
-app.get('/foo', routes.foo);
+app.get('/', middleware.all, middleware.index, routes.index);
 
-server.listen(8080);
-console.log("Listening on https://localhost:8080");
+app.get('/login', middleware.all, routes.login);
+app.post('/login', middleware.all, routes.postLogin);
+app.get('/logout', middleware.all, middleware.secure, routes.logout);
+
+app.get('/partials/:name', routes.partials);
+
+app.get('*', middleware.all, routes.index);
+
+server.listen(8080, function () {
+	console.log("Listening on https://localhost:8080");
+});
