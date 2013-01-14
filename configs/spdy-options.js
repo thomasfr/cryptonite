eebea1c5efd8b1edd6e8ee407acd0157cae1d99f
Thomas Fritz <fritztho@gmail.com>
2013-01-14 18:39:24 +0100

    Updated file and route setup

--- configs/spdy-options.js
+++ configs/spdy-options.js
@@ -1,12 +1,6 @@
-var spdy = require('spdy');
 var fs = require('fs');
-
-var spdyOptions = {
+module.exports = {
 	key: fs.readFileSync(__dirname + '/../keys/spdy-key.pem'),
 	cert: fs.readFileSync(__dirname + '/../keys/spdy-cert.pem'),
 	ca: fs.readFileSync(__dirname + '/../keys/spdy-csr.pem')
-};
-
-module.exports = function spdyServerSetup(app) {
-	return spdy.createServer(spdyOptions, app);
 }
